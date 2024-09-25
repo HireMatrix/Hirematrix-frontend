7381f41f051249bf4dd544314621b52c8d361cdb
mrshinchan1122 <saigane1122@gmail.com>
2024-09-26 00:05:15 +0530

    resume-builder-update1-html-css

--- src/pages/ResumeBuilderPage.jsx
+++ src/pages/ResumeBuilderPage.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const ResumeBuilderPage = () => {
   return (
-    <div>
-      ResumeBuilderPage
+    <div className='resume-builder-page-main-container'>
+      ResumeBuilderPage 
+      {/* ----------Hero Section ------------------ */}
+      <header className = "resumeBHead">
+        <h1 className = "Rh1">Career Compass</h1>
+        <nav id={Rnav}>
+            <ul id={Rul}>
+                <li className = "Rli"><a href="#" class="active">Resumes</a></li>
+                <li className = "Rli"><a href="#">Cover Letters</a></li>
+            </ul>
+        </nav>
+    </header>
+    {/* --------------- Main Section ------------------ */}
+    <main>
+        <div className="content">
+            <img src="resume-icon.png" alt="Resume Icon" class="icon"></img>
+            <h2>Create your best resume to advance career</h2>
+            <p>Build from scratch or pre-fill it with LinkedIn, or Apna profile</p>
+            <button class="create-btn">+ Create new resume</button>
+        </div>
+    </main>
     </div>
+    
   )
 }
 
